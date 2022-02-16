c29955dcca397280853baddd4d89060517da4515
MarahAlrefaai <marahalrefaai17@gmail.com>
2022-02-16 21:48:50 +0200

    task 19 not solved yet

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
-
+//task19
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>,
   </React.StrictMode>,
   document.getElementById('root')
 );
